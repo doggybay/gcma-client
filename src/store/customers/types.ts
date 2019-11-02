466f2b1d0efe3237d6ee2f55761f957a22acec31
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-02 14:00:45 -0700

    created fetch all types

---
+++ src/store/customers/types.ts
@@ -0,0 +1,40 @@
+import { FetchAllCustomersSuccessAction } from './types';
+import { FETCH_ALL_CUSTOMERS_PENDING, FETCH_ALL_CUSTOMERS_SUCCESS, FETCH_ALL_CUSTOMERS_FAILED } from './constants';
+
+export interface Customer {
+  id: number
+  name: string
+  company: string
+  email: string
+  phone: string
+  address: string
+  created_at: string
+  updated_at: string
+}
+
+export interface CustomerState {
+  customers: Customer[]
+}
+
+//All Customer Actions
+
+//Fetch All Customers
+export interface FetchAllCustomersPendingAction {
+  type: typeof FETCH_ALL_CUSTOMERS_PENDING
+}
+export interface FetchAllCustomersSuccessAction {
+  type: typeof FETCH_ALL_CUSTOMERS_SUCCESS
+  payload: Customer[]
+}
+export interface FetchAllCustomersFailedAction {
+  type: typeof FETCH_ALL_CUSTOMERS_FAILED
+  payload: any
+}
+
+
+//Group the different types of actions
+type FetchAllCustomersType = | FetchAllCustomersPendingAction | FetchAllCustomersSuccessAction | FetchAllCustomersFailedAction
+
+
+
+export type CustomerActionType = | FetchAllCustomersType
