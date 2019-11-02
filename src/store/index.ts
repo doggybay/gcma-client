9b645234953b746d18b6e5e1cfd0329467ce6044
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-02 14:36:54 -0700

    added customer reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,17 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux'
+
+//dev tools
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const rootReducer = combineReducers({
+//individual reducers
+import customers from './customers/reducer'
 
-})
+//combined reducer
+const rootReducer = combineReducers({ customers })
 
+//middleware array
 const middleware = [thunk, logger]
 
 export default createStore(rootReducer, composeWithDevTools(applyMiddleware(...middleware)))
