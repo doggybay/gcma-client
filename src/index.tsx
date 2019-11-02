6d6f960181589a941d562d246eece0022194c9c3
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-02 15:33:03 -0700

    added store

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'semantic-ui-css/semantic.min.css'
 import App from './App';
 
+//redux store
+import store from './store'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//provider component
+import { Provider } from 'react-redux'
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'))
 
