1f0f974393596d3045be497af42d354122ba09d4
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-02 15:33:56 -0700

    added fetch call

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
-import './App.css';
+import './App.css'
+
+import * as actions from './store/customers/actionCreators'
 
 const App: React.FC = () => {
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(actions.fetchAllCustomers())
+  })
   return (
     <div className="App">
       
     </div>
-  );
+  )
 }
 
 export default App;
