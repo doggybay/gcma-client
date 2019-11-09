ef6ea0947093bc65aec0bea4eade3888488a94a6
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-09 00:33:41 -0700

    working on teetimes list

---
+++ src/components/teeTimes/TeeTimesList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { List } from 'semantic-ui-css'
+
+import { TeeDate } from "./TeeDate";
+import { TeeTime } from "./TeeTime";
+
+export const TeeTimesList = ({ time }) => {
+  console.log(time)
+  //let timeToFormat = new Date(props.teeTime.time);
+
+  return (
+    <div></div>
+  );
+}
+
+//export default TeeTimesList
