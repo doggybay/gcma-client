bf7067a66b6f73b6fec0cdde57009f39802fde87
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-09 17:15:02 -0700

    working on list

--- src/components/teeTimes/TeeTimesList.js
+++ src/components/teeTimes/TeeTimesList.js
@@ -1,16 +1,35 @@
 import React from 'react'
-import { List } from 'semantic-ui-css'
+import { List, Container } from 'semantic-ui-css'
 
-import { TeeDate } from "./TeeDate";
-import { TeeTime } from "./TeeTime";
+import TDate from "./teeTime/TDate";
+import TTime from "./teeTime/TTime";
 
-export const TeeTimesList = ({ time }) => {
-  console.log(time)
-  //let timeToFormat = new Date(props.teeTime.time);
+const TeeTimesList = (props) => {
+  const { teeTimesArr } = props
+
+  const listOfTeeTimes = teeTimesArr.map(teeTime => {
+    const timeToFormat = new Date(teeTime.time);
+    return (
+      <div class="content">
+        <div class="header">Scheduled teetime for:</div>
+        <div class="description">
+          <TDate time={timeToFormat} />  at <TTime time={timeToFormat} />
+        </div>
+      </div>
+    );
+  });
 
   return (
-    <div></div>
+    <div className="ui card">
+      {listOfTeeTimes}
+    </div>
   );
 }
 
-//export default TeeTimesList
+export default TeeTimesList
+
+// <List.Item key="teeTime.id" as="ol">
+//       <List.Item as="li">
+//         <TDate time={timeToFormat} /> at <TTime time={timeToFormat} />
+//       </List.Item>
+//     </List.Item>
