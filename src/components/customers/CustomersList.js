c82f421e5aea41be6112dff6f3df10418c34bc8b
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-11-09 17:14:40 -0700

    working on customers

--- src/components/customers/CustomersList.js
+++ src/components/customers/CustomersList.js
@@ -1,49 +1,21 @@
 import React from 'react'
-import { Image, Item, Container, List } from "semantic-ui-react";
+import { Container} from "semantic-ui-react";
 import { useSelector } from 'react-redux';
 
-import { TeeDate } from '../teeTimes/TeeDate'
-import { TeeTime } from '../teeTimes/TeeTime'
-import {TeeTimesList} from '../teeTimes/TeeTimesList'
+import Customer from './customer/Customer'
+
 
 const CustomersList = () => {
-  const customers = useSelector(state => state.customers.all);
-  
+  const customers = useSelector(state => state.customers.all)
 
-  const listOfCustomers = customers.map(customer => {
-
-    const teeTimes = customer.tee_times.map(teeTime => {
-
-      const timeToFormat = new Date(teeTime.time)
-      
-      return (
-        <List.Item key="" as="li" value="-">
-          <TeeDate time={timeToFormat} />
-          <> at</> <TeeTime time={timeToFormat} />
-        </List.Item>
-      );
-    })
-    
-    return (
-      <List.Item as="li" value="*">
-        {customer.name}
-        <List.Item as="ol">
-          {teeTimes}
-        </List.Item>
-      </List.Item>
-    );
-  })
+  const listOfCustomers = customers.map(customer => (<Customer customer={customer} />))
 
   return (
-  
     <Container>
       
-        <List as="ol">
-          {listOfCustomers}
-        </List>
-      
+      {listOfCustomers}
     </Container>
-  )
+  );
 };
 
 export default CustomersList;
---
+++ src/components/customers/customer/Customer.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Card, List } from "semantic-ui-react";
+
+import TeeTimesList from '../../teeTimes/TeeTimesList'
+
+const Customer = ({ customer }) => {
+
+  return (
+    <Card>
+      <Card.Content>
+        <Card.Header>{customer.name}</Card.Header>
+        <Card.Meta>
+          <span className="date">{customer.email}</span>
+        </Card.Meta>
+        <Card.Description as="ol">{customer.company}</Card.Description>
+      </Card.Content>
+      <Card.Content>
+        <TeeTimesList teeTimesArr={customer.tee_times} />
+      </Card.Content>
+    </Card>
+  );
+}
+
+export default Customer
+
+//<TeeTimes teeTime={teeTimes} />
