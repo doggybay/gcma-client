f287ccf24340cb57ae110f323347daf69be3a433
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2019-12-03 20:07:55 -0700

    working on tee times

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, createRef } from 'react'
 import { useDispatch } from 'react-redux'
-import { Container } from 'semantic-ui-react'
+import { Container, Grid, Segment, Sticky, Ref } from 'semantic-ui-react'
 import './App.css'
 
 import * as customerActions from './store/customers/actionCreators'
@@ -9,10 +9,16 @@ import * as teetimesActions from './store/teetimes/actionCreators'
 import CustomersList from './components/customers/CustomersList'
 import TeeTime from './components/teeTimes/teeTime/TeeTime'
 import TeeTimeList from './components/ttimes/TeeTimeList'
+import AvailTimes from './components/ttimes/AvailTimes'
+
+//Layout 
+import TopNav from './components/layout/TopNav'
+import SideNav from './components/layout/SideNav'
 
 
 const App: React.FC = () => {
   const dispatch = useDispatch()
+  const innerRef = createRef()
 
   useEffect(() => {
     dispatch(customerActions.fetchAllCustomers())
@@ -22,12 +28,27 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
-    
-        <TeeTimeList />
-      
-      
+      <TopNav />
+      <Ref innerRef={innerRef}>
+        <Grid columns={2} divided>
+          <Grid.Row stretched>
+            <Grid.Column width={3}>
+              <Sticky context={innerRef}>
+                <Segment>
+                  <SideNav />
+                </Segment>
+              </Sticky>
+            </Grid.Column>
+            <Grid.Column>
+              <Segment>
+                <AvailTimes />
+              </Segment>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Ref>
     </div>
-  )
+  );
 }
 
 export default App;
